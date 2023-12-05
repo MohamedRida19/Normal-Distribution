c7fa703a3561581a4ae6500c73f56e908dcde8eb
Mohamed Rida <rayanereda90@gmail.com>
2023-12-05 11:57:47 +0000

    completing the ValueError and KeyboardINterrupt

--- ADV_NOR_DIST.py
+++ ADV_NOR_DIST.py
@@ -46,70 +46,69 @@ try:
     #adding for in loop based on the user attempts
     for i in range(user_attempts):
         #ask the user if he want to change the options
-        change_options = str(input("do you want to change the options (yes/no)?\n")).lower().split()
+        change_options = str(input("do you want to change the options (yes/no)?\n"))
         #get the inputs from the user if he wants to change the options
-        try:
-            if change_options== "yes":
-                time.sleep(0.3)
-                print("Starting with: Graph options")
-                loc_var = numeric_user_input("I want the value of loc (where is the bell start, recommended 0):\n", default = default_loc_var)
-                scale_var = numeric_user_input("I want the value of scale(how flat is the graph, recommende 1):\n", default = default_scale_var)
+        if change_options== "yes":
+            time.sleep(0.3)
+            print("Starting with: Graph options")
+            loc_var = numeric_user_input("I want the value of loc (where is the bell start, recommended 0):\n", default = default_loc_var)
+            scale_var = numeric_user_input("I want the value of scale(how flat is the graph, recommende 1):\n", default = default_scale_var)
                 
-                graph_color = str(input("Choose the color of the graph (skip this by pressing enter)\n")).strip()
-                print_loading_message("Cheking the input!", "Done!")
-                if not graph_color:
-                    print("Default options applied!")
-                    graph_color = default_graph_color
-                elif not graph_color.replace('.','').isdigit():
-                    print("Customizing options applied!")
-                    pass
-                else:
-                    print('What! Anyways default settings applied.')
-                
-                time.sleep(0.3)
-                print("Moving on: text options")
-                time.sleep(0.1)
-                font_type = str(input("you can set the font type (skip this by pressing enter):\n")).lower().strip()
-                print_loading_message("Cheking the input!", "Done!")
-                if not font_type:
-                    print("default options applied!")
-                    font_type = default_font_type
-                elif not font_type.replace('.','').isdigit():
-                    print("Customizing options applied!")
-                    pass
-                else:
-                    print("What? Anyways default settings applied")
-                
-                font_size = numeric_user_input("set the font size pls(recommended 16pts):\n", default = default_font_size)
-
-                font_color = str(input("you can set the font color (skip this by pressing etner):\n")).lower().strip()
-                print_loading_message("Checking the input!", "Done!")
-                if not font_color:
-                    print("Default options applied!")
-                    font_color = default_font_color
-                elif not font_color.replace('.','').isdigit():
-                    print("Customizing options applied!")
-                    pass
-                else :
-                    print("What! Anyways default settings applied.")
-            elif change_options == 'no':
-                print("Default settings applied!")
+            graph_color = str(input("Choose the color of the graph (skip this by pressing enter)\n")).strip()
+            print_loading_message("Cheking the input!", "Done!")
+            if not graph_color:
+                print("Default options applied!")
+                graph_color = default_graph_color
+            elif not graph_color.replace('.','').isdigit():
+                print("Customizing options applied!")
                 pass
             else:
-                print("I cannot understand this! default settings applied!")    
+                print('What! Anyways default settings applied.')
+                
+            time.sleep(0.3)
+            print("Moving on: text options")
+            time.sleep(0.1)
+            font_type = str(input("you can set the font type (skip this by pressing enter):\n")).strip()
+            print_loading_message("Cheking the input!", "Done!")
+            if not font_type:
+                print("default options applied!")
+                font_type = default_font_type
+            elif not font_type.replace('.','').isdigit():
+                print("Customizing options applied!")
                 pass
-            #adding the figure's code
-            print_loading_message("Calculating DATA!", "Creating new figure done!") 
-            data = np.random.normal(loc = loc_var if change_options == "yes" else default_loc_var , scale= scale_var if change_options == "yes" else default_scale_var, size= 1000)
-            sns.histplot(data, kde = True, linewidth = 0, color = graph_color if change_options == "yes" else default_graph_color)
-            #adding the title to the figure
-            text_options = {'font_type' : font_type if change_options == "yes" else default_font_type, 'font_size' : font_size if change_options == "yes" else default_font_size, 'font_color' : font_color if change_options == "yes" else default_font_color}
-            plt.title("NORMAL DISTRIBUTION", **text_options)
-            plt.show()
-        except ValueError as ve:
-            print()
-except ValueError as ve:
-    print()
+            else:
+                print("What? Anyways default settings applied")
+                
+            font_size = numeric_user_input("set the font size pls(recommended 16pts):\n", default = default_font_size)
 
+            font_color = str(input("you can set the font color (skip this by pressing etner):\n")).strip()
+            print_loading_message("Checking the input!", "Done!")
+            if not font_color:
+                print("Default options applied!")
+                font_color = default_font_color
+            elif not font_color.replace('.','').isdigit():
+                print("Customizing options applied!")
+                pass
+            else :
+                print("What! Anyways default settings applied.")
+        elif change_options == 'no':
+            print("Default settings applied!")
+            pass
+        else:
+            print("I cannot understand this! default settings applied!")    
+            pass
+        #adding the figure's code
+        print_loading_message("Calculating DATA!", "Creating new figure done!") 
+        data = np.random.normal(loc = loc_var if change_options == "yes" else default_loc_var , scale= scale_var if change_options == "yes" else default_scale_var, size= 1000)
+        sns.histplot(data, kde = True, linewidth = 0, color = graph_color if change_options == "yes" else default_graph_color)
+        #adding the title to the figure
+        text_options = {'font' : font_type if change_options == "yes" else default_font_type, 'size' : font_size if change_options == "yes" else default_font_size, 'color' : font_color if change_options == "yes" else default_font_color}
+        plt.title("NORMAL DISTRIBUTION", **text_options)
+        plt.show()
+
+except ValueError as ve:
+    print(f"Incorrect input: {ve}. Pls try again and enter a postif integer value!")
+    exit()
 except KeyboardInterrupt as kbi:
-    print()
+    print(f"Something wrong: {kbi}. Pls try again!")
+    exit()
