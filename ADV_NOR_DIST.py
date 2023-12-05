0adfd51f747f9b003b837a7c0b77591f258d2f98
Mohamed Rida <rayanereda90@gmail.com>
2023-12-05 11:01:59 +0000

    coding the part if the user wants to change the parametres

--- ADV_NOR_DIST.py
+++ ADV_NOR_DIST.py
@@ -40,15 +40,61 @@ time.sleep(0.5)#waiting 500ms befor passing this line
 #set the try except block for handling errors
 try:
     #get from the user the number of attepmts
-    user_attempts = int(input('how many times you want to try this? (only positif integer numbers)'))
+    user_attempts = int(input('how many times you want to try this? (only positif integer numbers)\n'))
     print_loading_message("Checking the input!", "Done!")
     i = 0
     #adding for in loop based on the user attempts
     for i in range(user_attempts):
         #ask the user if he want to change the options
-        change_options = str(input("do you want to change the options (yes/no)?")).split()
-        
+        change_options = str(input("do you want to change the options (yes/no)?\n")).lower().split()
+        #get the inputs from the user if he wants to change the options
+        try:
+            if change_options== "yes":
+                time.sleep(0.3)
+                print("Starting with: Graph options")
+                loc_var = numeric_user_input("I want the value of loc (where is the bell start, recommended 0):\n", default = default_loc_var)
+                scale_var = numeric_user_input("I want the value of scale(how flat is the graph, recommende 1):\n", default = default_scale_var)
+                
+                graph_color = str(input("Choose the color of the graph (skip this by pressing enter)\n")).strip()
+                print_loading_message("Cheking the input!", "Done!")
+                if not graph_color:
+                    print("Default options applied!")
+                    graph_color = default_graph_color
+                elif not graph_color.replace('.','').isdigit():
+                    print("Customizing options applied!")
+                    pass
+                else:
+                    print('What! Anyways default settings applied.')
+                
+                time.sleep(0.3)
+                print("Moving on: text options")
+                time.sleep(0.1)
+                font_type = str(input("you can set the font type (skip this by pressing enter):\n")).lower().strip()
+                print_loading_message("Cheking the input!", "Done!")
+                if not font_type:
+                    print("default options applied!")
+                    font_type = default_font_type
+                elif not font_type.replace('.','').isdigit():
+                    print("Customizing options applied!")
+                    pass
+                else:
+                    print("What? Anyways default settings applied")
+                
+                font_size = numeric_user_input("set the font size pls(recommended 16pts):\n", default = default_font_size)
 
+                font_color = str(input("you can set the font color (skip this by pressing etner):\n")).lower().strip()
+                print_loading_message("Checking the input!", "Done!")
+                if not font_color:
+                    print("Default options applied!")
+                    font_color = default_font_color
+                elif not font_color.replace('.','').isdigit():
+                    print("Customizing options applied!")
+                    pass
+                else :
+                    print("What! Anyways default settings applied.")
+                
+        except ValueError as ve:
+            print()
 except ValueError as ve:
     print()
 
