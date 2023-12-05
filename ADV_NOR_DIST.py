9d4950c39a3e0c253c1013d1c01eab8f9b71a3c3
Mohamed Rida <rayanereda90@gmail.com>
2023-12-05 09:53:36 +0000

    creating loading function

--- ADV_NOR_DIST.py
+++ ADV_NOR_DIST.py
@@ -11,3 +11,12 @@ default_graph_color = "blue"
 default_font_type = "Arial"
 default_font_size = 16
 default_font_color = "red"
+
+#creating loading function
+def print_loading_message(message):
+    print(message, end='', flush= False)
+    for i in range(3):
+        time.sleep(0.2)
+        print('.', end=' ', flush=False)
+    time.sleep(0.1)
+    print(message)
