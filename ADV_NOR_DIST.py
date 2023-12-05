e33c3206c14178daebc2b3100964a86ccd2fac0c
Mohamed Rida <rayanereda90@gmail.com>
2023-12-05 09:48:52 +0000

    setting the defaults values

--- ADV_NOR_DIST.py
+++ ADV_NOR_DIST.py
@@ -3,3 +3,11 @@ import numpy as np
 import seaborn as sns
 import time
 import matplotlib.pyplot as plt
+
+#set the default values
+default_loc_var = 0
+default_scale_var = 1
+default_graph_color = "blue"
+default_font_type = "Arial"
+default_font_size = 16
+default_font_color = "red"
